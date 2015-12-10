0a8b857b38a5e69c88211c50921edb316ad01ecc
mac-guru <amritsapkota11@gmail.com>
2015-12-10 18:36:42 +0545

    Initial Commit

---
+++ tabBarApplication/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  tabBarApplication
+//
+//  Created by amrit on 12/10/15.
+//  Copyright © 2015 Amrit. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
